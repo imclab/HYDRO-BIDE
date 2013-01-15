2716243806851733d7cbc644521f4a2230421e6a
klocey <locey@biology.usu.edu>
2013-01-14 19:04:17 -0700

    Update hydrobide.py
    Shortened code -KL

--- hydrobide.py
+++ hydrobide.py
@@ -24,68 +24,41 @@ from random import randrange
      chemostat/organ/bioreactor (COB) scenario. There is 1 source, 1 community, and
      1 outlet. This model incorporates dormancy and per capita resource limitation.
      The eventual goal will be to include species, taxa, and trait differences.
-     Accounting for temperature and pH would be fun, too.
+     The model is spatially implicit
      
-     The model is spatially implicit, which makes sense because COBs are often
-     well-mixed and we have no interest in tracking individual bacteria
-     
-     Volume (V), influent rate (r), and hence residence time (V/r) are modeled
-     as influencing multiple resource and community-related rates and dynamics.
-     The incoming concentration of resources and density of propagules are also
-     accounted for. Influent rate (r) is the constraining rate on all passively
-     in-flowing stuff. 
+     Volume (V) and influent rate (r) are modeled as influencing multiple resource
+     and community-related rates and dynamics. The incoming concentration of
+     resources and density of propagules are also accounted for. Influent rate (r)
+     is the constraining rate on all passively in-flowing stuff. 
 
-     Unlike some neutral models, this model is not zero-sum, i.e. community size (N)     
-     is not fixed and can fluctuate wildly. As in real COBs, the community should
-     go extinct if the inflow rate (r) is to high. Likewise, N should grow to
-     capacity if r is to low and if the resource concentration is to high. 
-     Like any good model, this model should yield realistic behavior.
-     
-     There is no mutation rate and no speciation, but there could be.
-     Unlike any other ecological neutral model, individuals do not have to be
-     'born' into a state of reproductive maturity. Individuals are modeled such
-     that they can grow to a reproductively viable state. The rate of per capita
-     growth is then influenced by resource availability (R) and R is influenced
-     by V, r, and the concentration of resources in the inflowing medium. 
-
-     This script will eventually be carved up into more manageable pieces, but
-     at the moment, I'm just plowing through. """     
+     This model is not zero-sum, i.e. community size (N) is not fixed and can
+     fluctuate wildly. There is no mutation rate and no speciation, but there could be.
+     Individuals are modeled such that they can grow to a reproductively viable state.
+     The rate of per capita growth is then influenced by resource availability (R)
+     and R is influenced by V, r, and the concentration of resources in the inflowing medium. 
+     In this way, the model is similar to the Monod growth model. """     
 
     
-V = 1000.0      # volume of the COB                                                                                                  
-r = 10.0        # influent rate (unit volume/unit time)                                                                  
-
-res_dens = 1.0 # growth limiting resource concentration of inflowing medium,
-                # e.g. (grams cellulose + grams x + grams y)/ (liter of inflowing medium) 
-
+V = 1000.0      # volume                                                                                           
+r = 10.0        # influent rate                                                             
+res_dens = 1.0 # growth limiting resource concentration of inflowing medium
 prop_dens = 1.0 # propagule density, (cells per unit volume of inflowing medium)                     
-                # Assume initially that resource concentration of the influent equals
-                # the resource concentration of the COB. This makes sense if we're 
-                # starting with a community of zero individuals.
 
 dorm_lim = 0.01  # dormancy threshold; dormancy is undertaken if per capita resource availability
-                # is below some threshhold (low resources -> low metabolism -> slow growth = go dormant)
-                # This could be made to vary among species
+bp = 0.5 # arbitrarily set binomial probability that a propagule is active
 
-bp = 0.5 # arbitrarily set binomial probability
-         # the probability that a propagule is active
+rates = hm.get_in_rates(r,prop_dens,res_dens) # get immigration rate and resource delivery rate
+im_rate = rates[0]  # immigration rate, (cells/unit time)                                     
+res_rate = rates[1] # resource delivery rate, (unit resource/unit time) 
 
-im_rate = int(round(prop_dens * r)) # immigration rate (cells/unit time)                                     
-res_rate = res_dens * r # resource delivery rate, (unit resource/unit time) 
-
-R = V * res_dens # Amount of resources in the COB before inoculation. If inflow = outflow and the COB is empty,
-                 # then the initial resource concentration would equal that of the inflow and outflow. Eventually,
-                 # total resources (R) will change as a result of a changing community.
+R = V * res_dens # Amount of resources in the environment before inoculation.
 
 """ The community (COBcom) will be a list of lists: """
 COBcom = []
-comlist = hm.immigration(COBcom, im_rate, bp,lgp = 0.7) # inoculate the community with propagules using the above function
+comlist = hm.immigration(COBcom, im_rate, bp,lgp = 0.7) # inoculate the community with propagules
 COBcom = comlist[0] # The community
 N = len(COBcom)     # size of the community
-num_A = comlist[1]  # number of active individuals in COBcom 
-                    # Each list will contain the individual species label and reveal whether the          
-                    # individual is dormant or active, and how close the individuals is to          
-                    # being reproductively viable.                                                  
+num_A = comlist[1]  # number of active individuals.                                      
 
 """ Things that will change as the community changes """         
 c = 1.0 # constant of proportionality; will eventually be used to create taxa differences
@@ -98,14 +71,11 @@ ind_res = R/num_A # per capita resource availability
 ind_grow = a*ind_res # proportion growth towards reproductive viability achieved per unit time (not constant)
                      # This makes per capita growth rate directly proportional to per capita resource availability.
                      # So, in general, individuals grow according to their share of resources
-                     
                 
-""" Having set up the community, it's time to turn it loose. From here on, community size, biomass,
-    dormancy, compositional and noncompositional community structure, population structure,
-    replacement, & turnover will all ride on random drift. """
+""" Having set up the community, it's time to turn it loose. """
     
 time = 400   # length of the experiment
-burnin = 300  # number of initial time steps to discard
+burnin = int(0.75*time)  # number of initial time steps to discard
 
 N_COBcom = []   # list to track N over time
 A_COBcom = []   # list to track number of active individuals over time
@@ -118,26 +88,21 @@ RAD_Alow = []
 RAD_Amedium = [] 
 
 t = 0
-while t <= time: # looping one time unit at a time
-    #ct = 0
-    #for i in COBcom:
-    #    if i[1] == 1: ct += 1
-    print 'time',t,'immigrants',im_rate,' ','size=',N,'per capita resources=',ind_res,'active',num_A#,ct
+while t <= time: 
+    #print 'time',t,'immigrants',im_rate,' ','size=',N,'per capita resources=',ind_res,'active',num_A#,ct
     
     """ inflow of individuals, i.e., immigration """
     comlist = hm.immigration(COBcom, im_rate, bp, lgp=0.7) # add some propagules to the community
     COBcom = comlist[0]
     
     """ recalculate parameter values """
-    N = len(COBcom)     # Total community size will increase
-    num_A += comlist[1]  # total number of active individuals may increase
-    
-    R += res_rate        # total resources increases
+    N = len(COBcom)     
+    num_A += comlist[1] 
+    R += res_rate        
     ind_res = R/num_A  # per capita resource availability may change
                        # according to a change in the active portion
                        # of the COB community
-    ind_grow = a*ind_res # per capita growth rate will change according
-                         # to a change in per capita resources
+    ind_grow = a*ind_res 
     
     """" The community responds to the inflow & changes """
     # Simulation should reflect that the flow of individuals and resources
@@ -146,7 +111,6 @@ while t <= time: # looping one time unit at a time
     for i, v in enumerate(COBcom):
         
         if v[1] == 1:  # if the individual is active
-            
             if ind_res <= dorm_lim: # if per capita resource availability <= dormancy threshold, 
                                     # then the individual can go dormant or starve and die
                 
@@ -168,9 +132,6 @@ while t <= time: # looping one time unit at a time
                     
                 else:
                     COBcom[i][2] += ind_grow # grow if not mature
-                    # individual growth decreases R, which decreases
-                    # per capita resource availability and
-                    # per capita growth rate
                     R -= ind_grow
                     if num_A != 0: ind_res = R/num_A
                     ind_grow = a*ind_res
@@ -179,23 +140,22 @@ while t <= time: # looping one time unit at a time
             if ind_res > dorm_lim: # if per capita resource availability > the dormancy threshold
                 COBcom[i][1] = 1 # go active
                 num_A += 1
+    
     """ outflow of individuals, i.e., death/emigration """
     N = len(COBcom)
     num_out = int(round(N/V * r)) # no. individuals lost per unit time
     num_a = 0
     if num_out >= 1:
         comlist = hm.death_emigration(COBcom, num_out)
-        COBcom = comlist[0] # the newly decreased community
-        num_a = comlist[1]  # account for the number of lost active individuals
+        COBcom = comlist[0] 
+        num_a = comlist[1]  
     
     """ recalculate parameter values """
-    N = len(COBcom)  # total community size will have decreased
-    num_A -= num_a # number of active individuals may decrease
-    R -= (R/V) * r # total resources decreases according to the
-                   # amount of resources lost to outflow 
-    ind_res = R/num_A  # per capita resource availability may change
-    ind_grow = a*ind_res # per capita growth rate may change
-    
+    N = len(COBcom)  
+    num_A -= num_a 
+    R -= (R/V) * r 
+    ind_res = R/num_A  
+    ind_grow = a*ind_res 
     
     if t >= burnin:# and t%10 == 0: # allow a burn-in
         N_COBcom.append(np.log(N)) # using natural logs when values can be enormous
@@ -214,12 +174,11 @@ while t <= time: # looping one time unit at a time
         else:
            RAD = hm.get_rad(COBcom)
            RAD_Amedium.append(RAD)
+
     t += 1
-    random.shuffle(COBcom) # randomize the community, prevent artifacts
-                           # from arising in the next iteration of the 
-                           # for loop
+    random.shuffle(COBcom) # randomize the community, prevent artifacts from arising due to list order
     """ Here, we have completed one time interval of inflow/outflow """
-#print len(RAD_Ahigh),len(RAD_Amedium),len(RAD_Alow)
+
 nRADs = 10
 if len(RAD_Ahigh) > nRADs: RAD_Ahigh = random.sample(RAD_Ahigh,nRADs) 
 if len(RAD_Amedium) > nRADs: RAD_Amedium = random.sample(RAD_Amedium,nRADs) 
@@ -228,12 +187,9 @@ if len(RAD_Alow) > nRADs: RAD_Alow = random.sample(RAD_Alow,nRADs)
 sets_of_RADS = [RAD_Ahigh,RAD_Amedium,RAD_Alow] # A list to hold lists that have captured the community
                                                 # at times of high, low, and medium activity
     
-""" Here, we end the experiment and the result is
-    a huge list of small lists. Each small list
-    looks something like [1, 2, 50.0] with the 
-    first index representing the taxa label, the
-    second index representing active/dormant, & the
-    third index representing % growth to reproductive
+""" Here, we end the experiment and the result is a huge list of small lists. Each small list
+    looks something like [1, 2, 50.0] with the first index representing the taxa label, the
+    second index representing active/dormant, & the third index representing % growth to reproductive
     viability. """
 
 # plot fig1
